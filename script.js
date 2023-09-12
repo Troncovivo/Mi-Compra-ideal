6edf92a6fa057592bc73cdfed87e6b5458d133a6
Troncovivo <troncovivo.ng@outlook.com>
2023-09-12 00:20:43 -0300

    Commit °3
    modo oscuro v1

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-/* 
-var darkToggle = document.querySelectorAll(".dark-toggle");
-var body = document.querySelectorAll("body");
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
+  const options = {
+    bottom: '32px', // distancia desde el borde inferior de la pantalla
+    right: '32px', // distancia desde el borde derecho de la pantalla
+    mixColor: '#fff', // color base para mezclar con el modo oscuro
+    backgroundColor: '#fff', // color de fondo de la página
+    buttonColorDark: '#100f2c', // color del botón en modo oscuro
+    buttonColorLight: '#fff', // color del botón en modo claro
+    saveInCookies: true, // guardar la preferencia del usuario en cookies
+    label: '🌓', // texto o emoji del botón
+    autoMatchOsTheme: true // adaptarse al tema preferido del sistema operativo
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
 
 
-darkToggle.addEventListener("click", function() {
- 
-  body.classList.toggle("dark-mode");
-});
- */
+  function toggleDarkMode() {
+    let root = document.documentElement;
+    let darkMode = root.style.getPropertyValue("--bg-color") == "white";
+    if (darkMode) {
+      root.style.setProperty("--bg-color", "black");
+      root.style.setProperty("--text-color", "white");
+      root.classList.add("dark-mode"); // agregar la clase .dark-mode
+    } else {
+      root.style.setProperty("--bg-color", "white");
+      root.style.setProperty("--text-color", "black");
+      root.classList.remove("dark-mode"); // quitar la clase .dark-mode
+    }
+  }
+  
+    
