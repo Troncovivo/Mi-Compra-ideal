237ff1087adde2157cfc38bd2e03887b53cfc876
Troncovivo <troncovivo.ng@outlook.com>
2023-09-11 22:45:05 -0300

    2°Commit
    -Agregada pagina Blog (Principal)
    -Cambios en index.html y style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+var darkToggle = document.querySelectorAll(".dark-toggle");
+var body = document.querySelectorAll("body");
+
+
+darkToggle.addEventListener("click", function() {
+ 
+  body.classList.toggle("dark-mode");
+});
+ */
